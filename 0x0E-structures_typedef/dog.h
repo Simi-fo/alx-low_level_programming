14b4aa042b19987f56304a295745727d92a605b8
simi-fo <similoluwa.folorunsho@stu.cu.edu.ng>
2023-04-17 17:13:04 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: first info
+ * @age: second info
+ * @owner: third info
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

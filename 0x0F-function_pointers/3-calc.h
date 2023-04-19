72fedad26c877aa012d3a623673f4088cfb4f2d6
simi-fo <similoluwa.folorunsho@stu.cu.edu.ng>
2023-04-19 14:27:32 -0700

    write a program that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,7 +3,6 @@
 
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
@@ -14,7 +13,7 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-int op_sub(int a,int b);
+int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
